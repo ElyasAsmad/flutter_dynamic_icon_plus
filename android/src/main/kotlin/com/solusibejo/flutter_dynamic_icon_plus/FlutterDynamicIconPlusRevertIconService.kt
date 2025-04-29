97be2a99015759dad93b2432b2f97a45bcd2bf41
Elyas Asmad <elyasasmadz@gmail.com>
2025-04-30 02:00:44 +0800

    fix: add logging to service lifecycle methods for better debugging

--- android/src/main/kotlin/com/solusibejo/flutter_dynamic_icon_plus/FlutterDynamicIconPlusRevertIconService.kt
+++ android/src/main/kotlin/com/solusibejo/flutter_dynamic_icon_plus/FlutterDynamicIconPlusRevertIconService.kt
@@ -3,13 +3,23 @@ package com.solusibejo.flutter_dynamic_icon_plus
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class FlutterDynamicIconPlusRevertIconService: Service() {
     override fun onBind(p0: Intent?): IBinder? = null
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_NOT_STICKY
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("IconRevertService", "Service started")
+        return START_NOT_STICKY
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("IconRevertService", "Service created")
+    }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
+        Log.d("IconRevertService", "Task removed - reverting icon")
         ComponentUtil.revertToDefaultIcon(this, packageManager)
 
         super.onTaskRemoved(rootIntent)
@@ -17,6 +27,7 @@ class FlutterDynamicIconPlusRevertIconService: Service() {
     }
 
     override fun onDestroy() {
+        Log.d("IconRevertService", "Service being destroyed - reverting icon")
         ComponentUtil.revertToDefaultIcon(this, packageManager)
 
         super.onDestroy()
--- android/src/main/kotlin/com/solusibejo/flutter_dynamic_icon_plus/FlutterDynamicIconPlusService.kt
+++ android/src/main/kotlin/com/solusibejo/flutter_dynamic_icon_plus/FlutterDynamicIconPlusService.kt
@@ -3,15 +3,24 @@ package com.solusibejo.flutter_dynamic_icon_plus
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class FlutterDynamicIconPlusService: Service() {
     override fun onBind(p0: Intent?): IBinder? = null
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_NOT_STICKY
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("IconService", "Service started")
+        return START_NOT_STICKY
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("IconService", "Service created")
+    }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
+        Log.d("IconService", "Task removed - changing icon")
         ComponentUtil.changeAppIcon(this, packageManager, packageName)
-
         ComponentUtil.removeCurrentAppIcon(this)
 
         super.onTaskRemoved(rootIntent)
@@ -19,8 +28,8 @@ class FlutterDynamicIconPlusService: Service() {
     }
 
     override fun onDestroy() {
+        Log.d("IconService", "Service being destroyed - changing icon")
         ComponentUtil.changeAppIcon(this, packageManager, packageName)
-
         ComponentUtil.removeCurrentAppIcon(this)
 
         super.onDestroy()
